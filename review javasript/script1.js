bdc0032908c65445aa90971a4aef63127c550ae2
setiawandani <danistwn.004@gmail.com>
2024-04-16 13:46:44 +0700

    review javascript

---
+++ review javasript/script1.js
@@ -0,0 +1,43 @@
+// /*const bilangan = 2;
+
+// for (let a = 1; a <= 100; a++) {
+//   let prima = true;
+//   if (a === 1) {
+//     prima = false;
+//   } else {
+//     for (let b = 2; b <= a; b++) {
+//       if (a % b === 0) {
+//         prima = false;
+//         break;
+//       }
+//     }
+//   }
+//   if (prima) {
+//     console.log(a + " adalah bilangan prima ");
+//   } else {
+//     console.log(a + "");
+//   }
+// }*/
+
+// Aconsole.log("Bilangan prima antara 1 dan 100:");
+
+// for (let num = 1; num <= 100; num++) {
+//     let prima = true;
+
+// if (num>1){
+//     for (let i = 2; i < num; i++) {
+//         if (num % i === 0) {
+//             prima = false;
+//             break;
+//         }
+//     }
+//     }else{
+//      prima = false;
+//     }
+
+//      if (prima) {
+//         console.log(num + " adalah bilangan prima");
+//     }else{
+//         console.log(num);
+//     }
+// }
---
+++ review javasript/script3.js
@@ -0,0 +1,6 @@
+let num =prompt ("masukan angka")
+    console.log("berikut tabel perkalian "+ num)
+
+for (let i=1; i<=10; i++){
+console.log(num + " * " + i + " = " + num*i);  
+}
